10c3694306d1f1b0b8cc6d40555a1347ea9713b9
Elchin Jafar <elcinceferli@gmail.com>
2023-04-05 16:58:41 +0400

    add contact form functionality & hamburger menu items

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -1,27 +1,52 @@
+import { useRef } from "react";
 import Header from "../components/header/Header";
 import Button from "../components/button/Button";
+import emailjs from "@emailjs/browser";
+import { message } from "antd";
 import classes from "./Contact.module.css";
 
 function Contact() {
+  const form = useRef();
+  const [messageApi, contextHolder] = message.useMessage();
+
+  const sendEmail = (e) => {
+    e.preventDefault();
+
+    emailjs.sendForm('service_w5gtdzk', 'template_ep28gp2', form.current, 'zPR9eR7dAaFKp0_D7')
+      .then((result) => {
+          console.log(result.text);
+          console.log("message sent");
+          messageApi.open({
+            type: 'success',
+            content: 'Message Sent, Thanks!!'
+          });
+          e.target.reset();
+      }, (error) => {
+          console.log(error.text);
+      });
+  };
+
   return (
     <section id="contact" className={classes.contact}>
+      {contextHolder}
       <Header>contact</Header>
       <p className={classes["info-paragraph"]}>
         Feel free to Contact me by submitting the form below and I will get back
         to you as soon as possible
       </p>
       <div className={classes.form}>
-        <form action="">
+        <form ref={form} onSubmit={sendEmail}>
           <label htmlFor="name">Name</label>
-          <input type="text" id="name" placeholder="Enter Your Name" />
+          <input type="text" id="name" placeholder="Enter Your Name" name="from_name" />
           <label htmlFor="email">Email</label>
-          <input type="email" id="email" placeholder="Enter Your Email" />
+          <input type="email" id="email" placeholder="Enter Your Email" name="user_email" />
           <label htmlFor="message">Message</label>
           <textarea
             id="message"
             cols="30"
             rows="10"
             placeholder="Enter Your Message"
+            name="message"
           ></textarea>
           <Button mid>submit</Button>
         </form>
--- src/pages/Navbar.jsx
+++ src/pages/Navbar.jsx
@@ -1,15 +1,25 @@
-import React from "react";
+import React, { useState } from "react";
 import classes from "./Navbar.module.css";
 import { Sling as Hamburger } from 'hamburger-react'
 
 function Navbar() {
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+
+  function openMenu() {
+    setIsMenuOpen(true);
+  }
+
+  function closeMenu() {
+    setIsMenuOpen(false);
+  }
+
   return (
     <>
       <div className={classes.container}>
         <a href="#main" className={classes.logo}>
           Elchin Jafar
         </a>
-        <div className={classes.hamburger}>
+        <div className={classes.hamburger} onClick={() => setIsMenuOpen(prevState => !prevState)}>
           <Hamburger direction="left" size={20} />
         </div>
         <div className={classes.navlinks}>
@@ -18,6 +28,16 @@ function Navbar() {
           <a href="#projects">Projects</a>
           <a href="#contact">Contact</a>
         </div>
+        { isMenuOpen && <div className={classes.responsiveMenuContainer}>
+          <div className={classes.responsiveMenu} onClick={(e) => {
+              if(e.target.tagName === "A") setIsMenuOpen(false)
+          }}>
+            <a href="#main">Home</a>
+            <a href="#about">About</a>
+            <a href="#projects">Projects</a>
+            <a href="#contact">Contact</a>
+          </div>
+        </div>}
       </div>
     </>
   );
