445466aa81bc8cd14271f8d4e3ca2ff0ec687393
Elchin Jafar <elcinceferli@gmail.com>
2023-04-17 18:47:42 +0400

    new project added

--- src/components/project-element/ProjectElement.jsx
+++ src/components/project-element/ProjectElement.jsx
@@ -1,20 +1,16 @@
 import classes from "./ProjectElement.module.css";
 import Button from "../button/Button";
-import flagMac from "../../assets/dopefolio.jpeg";
+
 
 function ProjectElement({ img, title, info }) {
   return (
     <section className={classes["project-element"]}>
       <div className={classes.image}>
-        <img src={flagMac} alt="" />
+        <img src={img} alt="" />
       </div>
       <div className={classes.info}>
-        <h2 className={classes.header}>Flag App</h2>
-        <p className={classes["info-paragraph"]}>
-          Dopefolio is a successful Open-Source project that I created which
-          have been featured on some of the biggest tech sites like CSS-Tricks,
-          Hostinger, etc & used by thousands of developers globally
-        </p>
+        <h2 className={classes.header}>{title}</h2>
+        <p className={classes["info-paragraph"]}>{info}</p>
         <Button small>case study</Button>
       </div>
     </section>
--- src/pages/Projects.jsx
+++ src/pages/Projects.jsx
@@ -1,6 +1,11 @@
 import Header from "../components/header/Header";
 import ProjectElement from "../components/project-element/ProjectElement";
 import classes from "./Projects.module.css";
+import flagImg from "../assets/project-screenshots/flag.jpeg";
+import IgImg from "../assets/project-screenshots/is.png";
+import final from "../assets/project-screenshots/state.png";
+import big from "../assets/project-screenshots/big.png";
+import flagnation from "../assets/project-screenshots/flagnation.png"
 
 function Projects() {
   return (
@@ -10,7 +15,12 @@ function Projects() {
         Here you will find some of the personal and clients projects that I
         created with each project containing its own case study
       </p>
-      <ProjectElement />
+      <ProjectElement img={flagnation} title="Flag App" info="Dopefolio is a successful Open-Source project that I created which
+          have been featured on some of the biggest tech sites like CSS-Tricks,
+          Hostinger, etc & used by thousands of developers globally" />
+      <ProjectElement img={big} title="Instagram Clone" info="Dopefolio is a successful Open-Source project that I created which
+          have been featured on some of the biggest tech sites like CSS-Tricks,
+          Hostinger, etc & used by thousands of developers globally" />
     </section>
   );
 }
